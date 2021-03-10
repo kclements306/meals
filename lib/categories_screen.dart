cc4a37631f602dcf227f6dc5274232fdbf43c0cc
Ken Clements <kclements@techtonicgroup.com>
2021-03-10 08:36:31 -0700

    Starting with the App - Adding Base Data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
   }
 }
